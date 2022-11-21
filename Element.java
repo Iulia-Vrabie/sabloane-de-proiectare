00982d2455fc894d8807d35f2bd06c1ba0d1ce4d
Iulia-Vrabie <93011979+Iulia-Vrabie@users.noreply.github.com>
2022-11-21 20:00:36 +0200

    Update Element.java

--- Element.java
+++ Element.java
@@ -1,3 +1,10 @@
 public interface Element {
-	void print();
+	
+	public void print();
+	
+	public void add(Element element);
+	
+	public void remove(Element element);
+	
+	public Element get(int index);
 }
