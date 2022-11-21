181901ae70cb0aade2da27b236733365b67535cb
Iulia-Vrabie <93011979+Iulia-Vrabie@users.noreply.github.com>
2022-11-21 19:59:10 +0200

    Update Author.java

--- Author.java
+++ Author.java
@@ -1,14 +1,12 @@
 public class Author {
-	
 	String name;
-	
-	public Author (String name) {
-		this.name=name;
-	}
-	
-	public String getName() {
-		System.out.println(name);
-		return name;
-	}
+	String surname;
 
+	public Author(String name) {
+		this.name = name;
+	}
+	
+	public void print() {
+		System.out.println("Author: " + this.name);
+	}
 }
