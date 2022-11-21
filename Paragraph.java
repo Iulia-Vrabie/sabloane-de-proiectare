65931b1defd3c21f8364cb956e24553e908a8187
Iulia-Vrabie <93011979+Iulia-Vrabie@users.noreply.github.com>
2022-11-21 20:01:12 +0200

    Update Paragraph.java

--- Paragraph.java
+++ Paragraph.java
@@ -1,15 +1,29 @@
-public class Paragraph implements Element {
-	
+public class Paragraph implements Element{
 	String text;
-	
+
 	public Paragraph(String text) {
 		this.text = text;
 	}
+	
+	public void print() {
+		System.out.println("Paragraph: " + this.text);
+	}
 
 	@Override
-	public void print() {
-		System.out.println("Paragraph:" + text);
+	public void add(Element element) {
+		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public void remove(Element element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
