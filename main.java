31399bb413b11880d2fa24fd47bc1c3aa1d26668
Iulia-Vrabie <93011979+Iulia-Vrabie@users.noreply.github.com>
2022-11-21 20:02:49 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,27 +1,27 @@
-
 public class main {
 
 	public static void main(String[] args) {
-		
-		Book discoTitanic = new Book("Disco Titanic");
+		Book noapteBuna = new Book("Noapte buna, copii!");
 		Author rpGheo = new Author("Radu Pavel Gheo");
+		noapteBuna.addAuthor(rpGheo);
 		
-		discoTitanic.addAuthor(rpGheo);
-		int indexChapterOne = discoTitanic.createChapter("Capitolul 1");
-		Chapter chp1 = discoTitanic.getChapter(indexChapterOne);
-		int indexSubChapterOneOne= chp1.createSubChapter("Subcapitolul 1.1");
-		SubChapter scOneOne = chp1.getSubChapter(indexSubChapterOneOne);
+		Section cap1 = new Section("Capitolul 1");
+		Section cap11 = new Section("Capitolul 1.1");
+		Section cap111 = new Section("Capitolul 1.1.1");
+		Section cap1111 = new Section("Capitolul 1.1.1.1");
 		
-		scOneOne.createNewParagraph("Paragraph 1");
-		scOneOne.createNewParagraph("Paragraph 2");
-		scOneOne.createNewParagraph("Paragraph 3");
-		scOneOne.createNewParagraph("Paragraph 4");
-		scOneOne.createNewParagraph("Paragraph 5");
-		scOneOne.createNewImage("Image 1");
-		scOneOne.createNewTable("Table 1");
+		noapteBuna.addContent(new Paragraph("Multumesc celor care ..."));
+		noapteBuna.addContent(cap1);
+		cap1.add(new Paragraph("Moto capitol"));
+		cap1.add(cap11);
+		cap11.add(new Paragraph("Text from subchapter 1.1"));
+		cap11.add(cap111);
+		cap111.add(new Paragraph("Text from subchapter 1.1.1"));
+		cap111.add(cap1111);
+		cap1111.add(new Paragraph("Text from subchapter 1.1.1.1"));
+		cap1111.add(new Image("Image subchapter 1.1.1.1"));
 		
-		scOneOne.print();
-		
+		noapteBuna.print();
 	}
 
 }
