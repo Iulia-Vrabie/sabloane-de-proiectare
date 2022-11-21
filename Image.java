7c72fc5b140643788ababd3d892b69c0d568193b
Iulia-Vrabie <93011979+Iulia-Vrabie@users.noreply.github.com>
2022-11-21 20:00:51 +0200

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,13 +1,29 @@
-public class Image implements Element {
-	
-	String imageName;
+public class Image implements Element{
+	String url;
 
 	public Image(String imageName) {
-		this.imageName = imageName;
+		this.url = imageName;
 	}
-
+	
 	public void print() {
-		System.out.println("Image with name:" + imageName);
+		System.out.println("Image with name: " + this.url);
 	}
 
+	@Override
+	public void add(Element element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Element element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
